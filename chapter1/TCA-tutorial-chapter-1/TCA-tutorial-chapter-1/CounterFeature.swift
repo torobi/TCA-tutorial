6ad28dd2a0fdcd6c84313da1bdbfddc8c9243f30
torobi <torobi2222@gmail.com>
2026-01-06 22:07:20 +0900

    feat: your first featuer

---
+++ chapter1/TCA-tutorial-chapter-1/TCA-tutorial-chapter-1/CounterFeature.swift
@@ -0,0 +1,73 @@
+//
+//  CounterFeature.swift
+//  TCA-tutorial-chapter-1
+//
+//  Created by torobi on 2026/01/06.
+//
+
+import ComposableArchitecture
+import SwiftUI
+
+@Reducer
+struct CounterFeature {
+    @ObservableState
+    struct State {
+        var count = 0
+    }
+
+    enum Action {
+        case decrementButtonTapped
+        case incrementButtonTapped
+    }
+
+    var body: some Reducer<State, Action> {
+        Reduce { state, action in
+            switch action {
+            case .decrementButtonTapped:
+                state.count -= 1
+                return .none
+
+            case .incrementButtonTapped:
+                state.count += 1
+                return .none
+            }
+        }
+    }
+}
+
+struct CounterView: View {
+    let store: StoreOf<CounterFeature>
+
+    var body: some View {
+        VStack {
+            Text("\(store.count)")
+                .font(.largeTitle)
+                .padding()
+                .background(.black.opacity(0.1))
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+            HStack {
+                Button("-") {
+                    store.send(.decrementButtonTapped)
+                }
+                .font(.largeTitle)
+                .padding()
+                .background(.black.opacity(0.1))
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+
+                Button("+") {
+                    store.send(.incrementButtonTapped)
+                }
+                .font(.largeTitle)
+                .padding()
+                .background(.black.opacity(0.1))
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+            }
+        }
+    }
+}
+
+#Preview {
+    CounterView(store: Store(initialState: CounterFeature.State()) {
+        CounterFeature()
+    })
+}
--- chapter1/TCA-tutorial-chapter-1/TCA-tutorial-chapter-1/TCA_tutorial_chapter_1App.swift
+++ chapter1/TCA-tutorial-chapter-1/TCA-tutorial-chapter-1/TCA_tutorial_chapter_1App.swift
@@ -5,13 +5,18 @@
 //  Created by torobi on 2026/01/06.
 //
 
+import ComposableArchitecture
 import SwiftUI
 
 @main
 struct TCA_tutorial_chapter_1App: App {
+    static let store = Store(initialState: CounterFeature.State()) {
+        CounterFeature()._printChanges()
+    }
+
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            CounterView(store: TCA_tutorial_chapter_1App.store)
         }
     }
 }
