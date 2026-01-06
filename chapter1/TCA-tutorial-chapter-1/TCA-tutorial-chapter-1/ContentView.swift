b12c53dcc3fe97d846448e3f8cf1530cb0f522e3
torobi <torobi2222@gmail.com>
2026-01-06 20:39:35 +0900

    feat: create project

---
+++ chapter1/TCA-tutorial-chapter-1/TCA-tutorial-chapter-1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TCA-tutorial-chapter-1
+//
+//  Created by torobi on 2026/01/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ chapter1/TCA-tutorial-chapter-1/TCA-tutorial-chapter-1/TCA_tutorial_chapter_1App.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_tutorial_chapter_1App.swift
+//  TCA-tutorial-chapter-1
+//
+//  Created by torobi on 2026/01/06.
+//
+
+import SwiftUI
+
+@main
+struct TCA_tutorial_chapter_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
