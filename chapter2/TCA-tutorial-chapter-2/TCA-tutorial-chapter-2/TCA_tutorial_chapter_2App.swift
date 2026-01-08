caabba060d7e8a3f6e1f9ebd3e1f99b75106559f
torobi <torobi2222@gmail.com>
2026-01-08 22:14:39 +0900

    feat: create project

---
+++ chapter2/TCA-tutorial-chapter-2/TCA-tutorial-chapter-2/TCA_tutorial_chapter_2App.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_tutorial_chapter_2App.swift
+//  TCA-tutorial-chapter-2
+//
+//  Created by torobi on 2026/01/08.
+//
+
+import SwiftUI
+
+@main
+struct TCA_tutorial_chapter_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ chapter2/TCA-tutorial-chapter-2/TCA-tutorial-chapter-2Tests/TCA_tutorial_chapter_2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_tutorial_chapter_2Tests.swift
+//  TCA-tutorial-chapter-2Tests
+//
+//  Created by torobi on 2026/01/08.
+//
+
+import Testing
+@testable import TCA_tutorial_chapter_2
+
+struct TCA_tutorial_chapter_2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
